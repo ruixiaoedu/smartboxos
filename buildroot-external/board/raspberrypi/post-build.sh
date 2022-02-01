2db5fb9f42b617f600ea31575bbc6bf14dd79ca5
Harry Rong <harrykobe@gmail.com>
2022-02-02 02:06:16 +0800

    增加bash显示前缀

--- buildroot-external/board/raspberrypi/post-build.sh
+++ buildroot-external/board/raspberrypi/post-build.sh
@@ -4,4 +4,4 @@ set -u
 set -e
 
 # 开启getty登录支持
-"${HOST_DIR}/bin/systemctl" --root="${TARGET_DIR}" enable console-getty.service getty@tty1.service
+# "${HOST_DIR}/bin/systemctl" --root="${TARGET_DIR}" enable console-getty.service getty@tty1.service
---
+++ buildroot-external/rootfs-overlay/etc/profile.d/env.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# 设置bash的PS1的显示
+
+export PS1="\[\e[1;36m\][\u@\h \w]\\$\[\e[0m\]"
