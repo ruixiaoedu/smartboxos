fa883b624b44a8b752be133b543fd56955c06e65
Harry Rong <harrykobe@gmail.com>
2022-02-07 16:10:41 +0800

    取消开机启动的logo

--- buildroot-external/board/cm4sen/post-build.sh
+++ buildroot-external/board/cm4sen/post-build.sh
@@ -6,9 +6,9 @@ set -e
 BSP_INST_HOME_PATH=$(dirname $(realpath $0))
 
 # 复制必要文件
+cp $BSP_INST_HOME_PATH/boot-overlay/cmdline.txt ${BINARIES_DIR}/rpi-firmware/
 cp $BSP_INST_HOME_PATH/boot-overlay/dt-blob.bin ${BINARIES_DIR}/rpi-firmware/
 cp $BSP_INST_HOME_PATH/boot-overlay/overlays/*.dtbo ${BINARIES_DIR}/rpi-firmware/overlays/
-
 
 # 开启RTC功能
 "${HOST_DIR}/bin/systemctl" --root="${TARGET_DIR}" enable rtc.service
