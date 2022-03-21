3664aa61f02773194206f4558d0e74f90052f7ff
Harry Rong <harrykobe@gmail.com>
2022-03-21 12:14:23 +0800

    完善OTA功能

--- buildroot-external/package/ota/ota.mk
+++ buildroot-external/package/ota/ota.mk
@@ -1,19 +1,21 @@
-OTA_VERSION = 0.0.3
+OTA_VERSION = 0.0.4
 OTA_SITE = https://github.com/ruixiaoedu/ota/archive
 OTA_SOURCE = $(OTA_VERSION).tar.gz
 
+OTA_TAGS = autogen
 OTA_BUILD_TARGETS = cmd/ota
 OTA_GOMOD = github.com/ruixiaoedu/ota
 
 define OTA_INSTALL_CONFIG_FILES
 	$(INSTALL) -d -m 755 $(TARGET_DIR)/etc/ota
 
-	$(INSTALL) -D -m 0644 $(OTA_PKGDIR)/ota \
+	$(INSTALL) -D -m 0644 $(OTA_PKGDIR)/ota.conf \
 		$(TARGET_DIR)/etc/ota/ota.conf
-	$(INSTALL) -D -m 0644 $(OTA_PKGDIR)/server.crt \
-		$(TARGET_DIR)/etc/ota/server.crt
-
+	$(INSTALL) -D -m 0644 $(OTA_PKGDIR)/ota.crt \
+		$(TARGET_DIR)/etc/ota/ota.crt
 endef
+
+OTA_POST_INSTALL_TARGET_HOOKS += OTA_INSTALL_CONFIG_FILES
 
 define OTA_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 0644 $(OTA_PKGDIR)/ota.service \
